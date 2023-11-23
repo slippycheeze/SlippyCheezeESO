010e441e925d636e496e7fa74f8456ca4a1185d6
Daniel Pittman <daniel@rimspace.net>
2023-11-23 06:07:48 +0100

    initial commit of a personal ESO addon

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((buffer-file-coding-system . utf-8-dos))))
